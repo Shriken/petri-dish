3cd97a6dba83ed7309425996c2bfa322b99d29be
Zane Sterling <sterling.zane@gmail.com>
2015-12-04 12:47:05 -0500

    Add Genome.read

--- source/genome.d
+++ source/genome.d
@@ -1,5 +1,6 @@
 module genome;
 
+import std.conv;
 import std.stdio;
 import std.string;
 import std.algorithm;
@@ -82,4 +83,42 @@ void print(File file, Genome genome) {
 		file.writeln("color: ", mode.color);
 		file.writeln();
 	}
+}
+
+Genome read(ref Genome genome, string filename) {
+	genome.cellModes.destroy();
+	auto file = File(filename, "r");
+
+	int length;
+	file.readf("numModes: %s", &length);
+	foreach (i; 0 .. length) {
+		CellMode mode;
+		string cellType;
+		file.readf(" cellType: %s\n", &cellType);
+		mode.cellType = to!CellType(cellType);
+
+		file.readf(" makeAdhesin: %s", &mode.makeAdhesin);
+		file.readf(" nutrientPriority: %s", &mode.nutrientPriority);
+		file.readf(" splitThreshold: %s", &mode.splitThreshold);
+		file.readf(" splitRatio: %s", &mode.splitRatio);
+		file.readf(" splitAngle: %s", &mode.splitAngle);
+
+		file.readf(" child1Rotation: %s", &mode.child1Rotation);
+		file.readf(" child1Mode: %s", &mode.child1Mode);
+		file.readf(" child1KeepAdhesin: %s", &mode.child1KeepAdhesin);
+
+		file.readf(" child2Rotation: %s", &mode.child2Rotation);
+		file.readf(" child2Mode: %s", &mode.child2Mode);
+		file.readf(" child2KeepAdhesin: %s", &mode.child2KeepAdhesin);
+
+		file.readf(
+			" color: Color(%s, %s, %s)",
+			&mode.color.r,
+			&mode.color.g,
+			&mode.color.b
+		);
+		genome.cellModes ~= mode;
+	}
+
+	return genome;
 }
